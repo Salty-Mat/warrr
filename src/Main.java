5bcea68d1169ac1ff54f2536d93bb48157f714a2
Salty Mat <mathew.zahedi@gmail.com>
2023-02-03 11:35:44 -0800

    qqqqqqqqqqqq

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,38 @@ public class Main {
 
         deck.split();
 
+        //player char
+        char Picon;
+        char Pnum;
+        String Pboth;
+        //comp char
+        char Cicon;
+        char Cnum;
+        String Cboth;
+
         while (inGame){
+
             System.out.println("           < PLAYER >          < CPU >  ");
             System.out.println("            ? ——— ?            ? ——— ? ");
             System.out.println("            |  ?  |            |  ?  | ");
             System.out.println("            ? ——— ?            ? ——— ? ");
 
-            deck.battle();
+            System.out.println("\n         Press Enter to flip card...");
+            scanner.nextLine();
 
-            System.out.println("            ? ——— ?            ? ——— ? ");
-            System.out.println("            | "+deck.getPlayerCard()+" |  // VS //  | "+deck.getComputerCard()+" | ");
-            System.out.println("            ? ——— ?            ? ——— ? ");
+            deck.battle();
+            Picon = deck.getPlayerCard().toString().charAt(2);
+            Pnum = deck.getPlayerCard().toString().charAt(0);
+            Pboth = deck.getPlayerCard().toString();
+
+            Cicon = deck.getPlayerCard().toString().charAt(2);
+            Cnum = deck.getPlayerCard().toString().charAt(0);
+            Cboth = deck.getPlayerCard().toString();
+
+
+            System.out.println("            "+Picon+" ——— "+Pnum+"            "+Cicon+" ——— "+Cnum+" ");
+            System.out.println("            | "+ Pboth +" |  // VS //  | "+Cboth+" | ");
+            System.out.println("            "+Pnum+" ——— "+Picon+"            "+Cnum+" ——— "+Cicon+" ");
 
             deck.realBattle();
 
