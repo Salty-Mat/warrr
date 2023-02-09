d8d322ebcb7e72fd11e7b1c134eef88ecd80f521
Salty Mat <mathew.zahedi@gmail.com>
2023-02-09 11:36:20 -0800

    bruh

--- src/Main.java
+++ src/Main.java
@@ -20,9 +20,11 @@ public class Main {
         char Cnum;
         String Cboth;
 
-        scanner.nextLine();
+        System.out.println("Do you want to play in auto mode? (Y/n):");
+        String uhh = scanner.nextLine();
+        //System.out.println(uhh);
 
-        while (inGame){
+        while (inGame) {
 
             deck.battle();
             Picon = deck.getPlayerCard().toString().charAt(2);
@@ -33,49 +35,67 @@ public class Main {
             Cnum = deck.getComputerCard().toString().charAt(0);
             Cboth = deck.getComputerCard().toString();
 
-
-
-            System.out.println("           < PLAYER >          < CPU >  ");
-            System.out.println("            "+Picon+" ——— "+Pnum+"            "+Cicon+" ——— "+Cnum+" ");
-            System.out.println("            | "+ Pboth +" |  // VS //  | "+Cboth+" | ");
-            System.out.println("            "+Pnum+" ——— "+Picon+"            "+Cnum+" ——— "+Cicon+"\n");
+            System.out.println("           < PLAYER >          < CPU >  \n" + "            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " \n"+"            | " + Pboth + " |  // VS //  | " + Cboth + " | \n "+"           " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + "\n");
+           // System.out.println("           < PLAYER >          < CPU >  ");
+           //System.out.println("            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " ");
+            //System.out.println("            | " + Pboth + " |  // VS //  | " + Cboth + " | ");
+            //System.out.println("            " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + "\n");
 
             System.out.println("                   --------------           \n");
 
 
-
-
-
             int winner = deck.realBattle();
 
-            if(winner == 0 ){
-                System.out.println("            "+Picon+" ——— "+Pnum+"            "+Cicon+" ——— "+Cnum+" ");
-                System.out.println("            | "+ Pboth +" |  WINNER >  | "+Cboth+" | ");
-                System.out.println("            "+Pnum+" ——— "+Picon+"            "+Cnum+" ——— "+Cicon+" ");
-            } else if(winner == 1){
-                System.out.println("            "+Picon+" ——— "+Pnum+"            "+Cicon+" ——— "+Cnum+" ");
-                System.out.println("            | "+ Pboth +" |  < WINNER  | "+Cboth+" | ");
-                System.out.println("            "+Pnum+" ——— "+Picon+"            "+Cnum+" ——— "+Cicon+" ");
+            if (winner == 0) {
+                    System.out.println("            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " \n" +"            | " + Pboth + " |  WINNER >  | " + Cboth + " | \n" + "            " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + " ");
+                    //System.out.println("            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " ");
+//                    System.out.println("            | " + Pboth + " |  WINNER >  | " + Cboth + " | ");
+//                    System.out.println("            " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + " ");
+            } else if (winner == 1) {
+                System.out.println("            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " \n" + "            | " + Pboth + " |  < WINNER  | " + Cboth + " | \n" + "            " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + " ");
+                //System.out.println("            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " ");
+                //System.out.println("            | " + Pboth + " |  < WINNER  | " + Cboth + " | ");
+                //System.out.println("            " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + " ");
             } else {
-                System.out.println("            "+Picon+" ——— "+Pnum+"            "+Cicon+" ——— "+Cnum+" ");
-                System.out.println("            | "+ Pboth +" |  WARRRRRR  | "+Cboth+" | ");
-                System.out.println("            "+Pnum+" ——— "+Picon+"            "+Cnum+" ——— "+Cicon+" ");
+                System.out.println("            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " \n" + "            | " + Pboth + " |  WARRRRRR  | " + Cboth + " | \n" + "            " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + " ");
+
+                //System.out.println("            " + Picon + " ——— " + Pnum + "            " + Cicon + " ——— " + Cnum + " ");
+                //System.out.println("            | " + Pboth + " |  WARRRRRR  | " + Cboth + " | ");
+                //System.out.println("            " + Pnum + " ——— " + Picon + "            " + Cnum + " ——— " + Cicon + " ");
             }
 
-            System.out.println("\n    Player cards: "+ deck.getPlayerDeck().size()+ "   computer cards: "+ deck.getComputerDeck().size() +"\n");
+            System.out.println("\n    Player cards: " + deck.getPlayerDeck().size() + "   computer cards: " + deck.getComputerDeck().size() + "\n");
 
 
+            if (uhh.equals("n") || uhh.equals("N")) {
+                System.out.println("------------------Press Enter clear table------------------");
+                scanner.nextLine();
+            } else if (uhh.equals("y") || uhh.equals("Y")) {
+                System.out.println("------------------------------------------------------------");
+
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                System.out.println("--------------------------------------");
+
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            clearScreen();
 
 
-
-            System.out.println("------------------Press Enter clear table------------------");
-            scanner.nextLine();
-
-            if (deck.getPlayerDeck().size() == 0 ){
-                System.out.println("         - COMPUTER WON -");
+            if (deck.getPlayerDeck().size() == 0) {
+                System.out.println("            🎉🎉  - COMPUTER WON - 🎉🎉");
                 inGame = false;
             } else if (deck.getComputerDeck().size() == 1) {
-                System.out.println("         - PLAYER WON -");
+                System.out.println("                🎉🎉  - PLAYER WON - 🎉🎉");
                 inGame = false;
             }
 
@@ -88,6 +108,11 @@ public class Main {
 
         }
 
+    }
 
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
